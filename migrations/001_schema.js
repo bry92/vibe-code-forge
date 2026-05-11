ef6a11a4d1185457a09a0974ce6c474050c5ffb3
bry92 <pagebrian49@gmail.com>
2026-05-11 16:36:40 -0700

    Add migrations/001_schema.js

--- migrations/001_schema.js
+++ migrations/001_schema.js
@@ -1,12 +1,13 @@
 exports.up = (pgm) => {
-  pgm.createTable('items', {
+  pgm.createTable('bookmarks', {
     id: 'id',
-    name: { type: 'varchar(255)', notNull: true },
-    description: { type: 'text', default: '' },
+    title: { type: 'varchar(255)', notNull: true },
+    url: { type: 'text', notNull: true },
+    tag: { type: 'varchar(50)', default: '' },
     created_at: { type: 'timestamp', default: pgm.func('current_timestamp') }
   });
 };
 
 exports.down = (pgm) => {
-  pgm.dropTable('items');
+  pgm.dropTable('bookmarks');
 };
